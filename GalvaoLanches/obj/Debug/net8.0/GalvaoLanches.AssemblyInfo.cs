6a5373d5d992091139d04808acdb0a932c2d7d86
Galvas <galvaomercante@gmail.com>
2025-07-28 13:46:54 -0300

    Create: Populando a tabela Lanches

--- GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
+++ GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0d7c533a65905e23dbad7cd831d4d2263891fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660c5ae37c2f3eccfddba5f6221de127b3cc1b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
