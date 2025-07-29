8e2cb3a808b0edbbefbf89524bfb197be920100d
Galvas <galvaomercante@gmail.com>
2025-07-29 13:35:27 -0300

    Create: Class CarrinhoCompra

---
+++ GalvaoLanches/Models/CarrinhoCompra.cs
@@ -0,0 +1,8 @@
+﻿namespace GalvaoLanches.Models
+{
+    public class CarrinhoCompra
+    {
+        public int CarrinhoCompraId { get; set; }
+        public IEnumerable<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+    }
+}
--- GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
+++ GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf27c2c5792476a9e42e03972bd4d989d31a918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e82811c48dcf56ff71603531f560e4bb4275abbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
