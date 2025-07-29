6ae7e3eba17d916b6204442bee043796038b4cab
Galvas <galvaomercante@gmail.com>
2025-07-29 09:28:00 -0300

    Create: created controller and view List Lanches

---
+++ GalvaoLanches/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using GalvaoLanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GalvaoLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.lanches;
+            return View(lanches);
+        }
+    }
+}
--- GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
+++ GalvaoLanches/obj/Debug/net8.0/GalvaoLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5373d5d992091139d04808acdb0a932c2d7d86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705f893d96f87a36659107ffe2ab045890fc49a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalvaoLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
